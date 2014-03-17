2c3f7a330a116ff2a660a0a66751239465d65d76
saasbook <saasbook@saasbook.(none)>
2014-03-17 17:13:55 -0300

    Added rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratemylandlord::Application.config.secret_token = '9dc6e1bb617b5cb6d887d1dcb304828c81f8dc9d3ee017eae011f2dbc63ebd61a60e11410076244ccf2f9dcec802bb9b3182908050ebc45f773d148edad3b52c'
