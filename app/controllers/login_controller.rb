e9d27a4325f0341a0cd60ebadaf231134d2c76a3
saasbook <saasbook@saasbook.(none)>
2014-04-09 17:01:00 -0400

    Login stuff

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,9 +1,15 @@
 class LoginController < ApplicationController
   def login
     user = User.find_by_email(params[:login][:email])
-    unless user.nil?
+    if user.nil?
+      error
+    else
       pass = User.hash_pass(params[:login][:password])
-      session[:user] = user.email if user.password == pass
+      if user.password == pass
+        session[:user] = {id: user.id, email: user.email, permissions: user.permissions}
+      else
+        error
+      end
     end
     redirect_to params[:redirect]
   end
@@ -11,5 +17,9 @@ class LoginController < ApplicationController
   def logout
     session[:user] = nil
     redirect_to params[:redirect]
+  end
+
+  def error
+    flash[:loginerror] = "Invalid bmail or password"
   end
 end
