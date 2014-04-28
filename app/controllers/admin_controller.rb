d87f543129bc6b8edb8dacb040b06586ae8e60a6
heimowitz <saasbook@saasbook.(none)>
2014-04-28 19:00:00 -0300

    revoke method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,11 @@
 class AdminController < ApplicationController
   def index
-    @admins = User.where(:permissions => true);
+    @admins = User.where(:permissions => true)
+  end
+  def revoke
+    user = User.find(params[:id])
+    user.permissions = false
+    user.save
+    redirect_to(admin_path)
   end
 end
