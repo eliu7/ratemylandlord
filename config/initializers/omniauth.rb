1a8876a2632c73822a00e11eaaef17e371306bc3
saasbook <saasbook@saasbook.(none)>
2014-04-30 17:58:13 -0400

    Changed authentication stuff - now asks each time

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '91567460252.apps.googleusercontent.com', 'tNP3BE7y53BLKTp0mtd-ZWqW',
-           {client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}}}
+           client_options: {ssl: {ca_file: Rails.root.join('cacert.pem').to_s}},
+           prompt: 'select_account'
 end
