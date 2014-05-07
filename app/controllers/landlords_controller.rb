1d177f8c65e361e5fbbefd446265148d563e4aad
saasbook <saasbook@saasbook.(none)>
2014-05-07 18:34:02 -0300

    pagification is finally perfected

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -9,12 +9,12 @@ class LandlordsController < ApplicationController
 
   def show
     landlord_id = params[:id]
-    pagenum = (params[:page] || '1').to_i
+    @pagenum = (params[:page] || '1').to_i
     @mylandlord = Landlord.find(landlord_id)
-    @reviews = @mylandlord.ratings(pagenum)
+    @reviews = @mylandlord.ratings(@pagenum)
     @avg_reviews=@mylandlord.average_ratings
     numreviews =@mylandlord.ratingstotal
-    @pagetotal = (numreviews/10).ceil
+    @pagetotal = (numreviews/10.0).ceil
     @color_func = lambda do |rating|
       case rating
       when (4.5..5)
