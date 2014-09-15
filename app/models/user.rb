35073673e9e88301aaf558c685599b52a8417bbc
Kevin <kjohns19@binghamton.edu>
2014-09-15 15:01:43 -0400

    Admin's first login now works, admins can have non-bmail accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      return nil unless auth.info.email.include? '@binghamton.edu'
+    where(:email => auth.info.email).first_or_initialize.tap do |user|
+      return nil unless auth.info.email.include?('@binghamton.edu') || user.admin
       user.provider = auth.provider
       user.uid = auth.uid
       user.admin = false unless user.email
