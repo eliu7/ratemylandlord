fed180d9bff200a07ac357f72cbc70a2e1fcf905
eliu7 <eliu7@binghamton.edu>
2015-07-19 03:36:30 -0400

    fixed db inconsistency

--- db/migrate/20150508041528_change_ratings.rb
+++ db/migrate/20150508041528_change_ratings.rb
@@ -1,18 +1,18 @@
 class ChangeRatings < ActiveRecord::Migration
   def up
-  	rename_column :landlords, :avg_friendliness, :avg_professionalism
+    rename_column :landlords, :avg_friendliness, :avg_professionalism
     rename_column :landlords, :avg_availability, :avg_credibility
     rename_column :ratings, :general, :general_1
     rename_column :ratings, :helpfulness, :helpfulness_1
     rename_column :ratings, :friendliness, :professionalism_1
     rename_column :ratings, :availability, :credibility_1
-  	add_column :ratings, :general_2, :integer
-  	add_column :ratings, :helpfulness_2, :integer
-  	add_column :ratings, :professionalism_2, :integer
-  	add_column :ratings, :credibility_2, :integer
+    add_column :ratings, :general_2, :integer
+    add_column :ratings, :helpfulness_2, :integer
+    add_column :ratings, :professionalism_2, :integer
+    add_column :ratings, :credibility_2, :integer
 
-  	add_column :ratings, :oldreview, :boolean	
-  	Rating.find_each do |rating|
+    add_column :ratings, :oldreview, :boolean	
+    Rating.find_each do |rating|
       rating.general_2 = -1
       rating.helpfulness_2 = -1
       rating.professionalism_2 = -1
@@ -23,7 +23,7 @@ class ChangeRatings < ActiveRecord::Migration
   end
 
   def down
-  	rename_column :landlords, :avg_professionalism, :avg_friendliness
+    rename_column :landlords, :avg_professionalism, :avg_friendliness
     rename_column :landlords, :avg_credibility, :avg_availability
     rename_column :ratings, :general_1, :general
     rename_column :ratings, :helpfulness_1, :helpfulness
@@ -31,12 +31,12 @@ class ChangeRatings < ActiveRecord::Migration
     rename_column :ratings, :credibility_1, :availability
 
 
-  	remove_column :ratings, :general_2
-  	remove_column :ratings, :helpfulness_2
-  	remove_column :ratings, :professionalism_2
-  	remove_column :ratings, :credibility_2
+    remove_column :ratings, :general_2
+    remove_column :ratings, :helpfulness_2
+    remove_column :ratings, :professionalism_2
+    remove_column :ratings, :credibility_2
 
-  	remove_column :ratings, :oldreview
+    remove_column :ratings, :oldreview
   end
 
 end
